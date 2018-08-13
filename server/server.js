4fe10fb89198c0c08b1d18ebbf9ccb133df5f9b2
Carlos <clopezalcaraz@gmail.com>
2018-08-13 20:21:42 +0200

    changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -7,39 +8,17 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(require('./routes/usuario.js'))
 
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
+
+
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err;
+    console.log('BDD ONLINE');
 });
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({ persona: body });
-    }
-
-
-});
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
-
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando');
+    console.log(`Escuchando puerto ${process.env.PORT}`);
 });
